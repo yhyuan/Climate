f56bfdee7d87b7bf314b8673f39ed78a512a6390
Jerry Yuan <yhyuan@gmail.com>
2015-07-30 08:15:18 -0400

    Fix bugs

--- Step1.py
+++ Step1.py
@@ -1,94 +1,93 @@
+# This script downloads the weather station information from Canada National Climate Archives and save it in a text file named weather.txt. 
+# This script need to send the http request to outside network. Firewall may restrict the access. 
 import httplib, time
+import urllib2
+				  
+def parseLatlng(input):
+	items = input.split('<abbr title="degrees">&deg;</abbr>')
+	degree = int(items[0][items[0].rfind('<td>') + len('<td>'):])
+	items = items[1].split('<abbr title="minute">\'</abbr>')
+	minute = int(items[0])
+	second = float(items[1][:items[1].find('<abbr title="second">')])
+	return str(degree + minute/60.0 + second/3600.0)
 def processOnePage(data1):
 	results = []
 	items = data1.split('<div class="span-2 row-end row-start margin-bottom-none station  wordWrap">')
 	for i in range(1, len(items) - 1):
-		#StationName = items[i].split('</div> 				<div class="span-1 row-end row-start margin-bottom-none wordWrap">')[0].strip()
-		StationID = items[i].split('<input type="hidden" name="StationID" value="')[1].split('" />  	<input type="hidden" name="Prov" value="ONT" />')[0]
-		#print StationID
-		results.append(StationID)
-		#conn = httplib.HTTPConnection("climate.weather.gc.ca")
-		#conn.request("GET", "/climateData/bulkdata_e.html?format=csv&stationID=" + StationID + "&Year=2014&Month=1&Day=1&timeframe=2&submit=Download+Data")
-		#f = open (StationID + ".txt","w")
-		#f.write(conn.getresponse().read() + "\n")
-		#f.close()		
-		'''hlyRange = items[i].split('<input type="hidden" name="hlyRange" value="')[1].split('" /> 		<input type="hidden" name="dlyRange" value="')[0]
-		dlyRange = items[i].split('<input type="hidden" name="dlyRange" value="')[1].split('" />   	<input type="hidden" name="mlyRange" value="')[0]
-		mlyRange = items[i].split('<input type="hidden" name="mlyRange" value="')[1].split('" />  	<input type="hidden" name="StationID" value="')[0]
-		# http://climate.weather.gc.ca/climateData/dailydata_e.html?timeframe=2&Prov=ONT&StationID=4612&dlyRange=1931-01-01|1966-11-30&Year=1966&Month=11&Day=01
-		#conn = httplib.HTTPConnection("lrcdrrvsdvap002")
-		#conn.request("GET", "/web/page2.htm")
-		
-		#f = open (StationID + ".txt","w")
-		#f.write(StationName + "\n")
-		print StationID + ", " + StationName
-		#print (dlyRange == "|")
-		if (dlyRange == "|"):
-			#results.append([StationName, StationID, hlyRange, dlyRange, mlyRange])
-			#continue
-			if (hlyRange == "|"):
-				YMD = mlyRange.split("|")[1].split("-")
-			else:
-				YMD = hlyRange.split("|")[1].split("-")
+		StationID = items[i].split('<input type="hidden" name="StationID" value="')[1].split('" />      <input type="hidden" name="Prov" value="ON" />')[0]
+		dlyRange = items[i].split('<input type="hidden" name="dlyRange" value="')[1].split('" />      <input type="hidden" name="mlyRange" value="')[0]
+		hlyRange = items[i].split('<input type="hidden" name="hlyRange" value="')[1].split('" />              <input type="hidden" name="dlyRange" value="')[0]
+		mlyRange = items[i].split('<input type="hidden" name="mlyRange" value="')[1].split('" />      <input type="hidden" name="StationID" value="')[0]
+		print StationID + "\t" + hlyRange + "\t" + dlyRange + "\t" + mlyRange
+		if (hlyRange != "|"):
+			end = hlyRange.split('|')[1].split('-')
+			url = 'http://climate.weather.gc.ca/climateData/hourlydata_e.html?timeframe=1&Prov=ON&StationID=' + StationID + '&hlyRange=' + hlyRange + '&Year=' + end[0] + '&Month=' + str(int(end[1])) + '&Day=' + str(int(end[2]))
+		elif (dlyRange != "|"):
+			end = dlyRange.split('|')[1].split('-')
+			url = 'http://climate.weather.gc.ca/climateData/hourlydata_e.html?timeframe=2&Prov=ON&StationID=' + StationID + '&dlyRange=' + dlyRange + '&Year=' + end[0] + '&Month=' + str(int(end[1])) + '&Day=01'
+		elif (mlyRange != "|"):
+			end = mlyRange.split('|')[1].split('-')
+			url = 'http://climate.weather.gc.ca/climateData/hourlydata_e.html?timeframe=3&Prov=ON&StationID=' + StationID + '&mlyRange=' + mlyRange + '&Year=' + end[0] + '&Month=01&Day=01'
+			#f = open('output//' + StationID + "_mly.html", 'w+')
+			#f.write(urllib2.urlopen(url).read())
+			#f.close()
 		else:
-			YMD = dlyRange.split("|")[1].split("-")
-		conn = httplib.HTTPConnection("climate.weather.gc.ca")
-		conn.request("GET", "/climateData/dailydata_e.html?timeframe=2&Prov=ONT&StationID=" + StationID + "&dlyRange=" + dlyRange + "&Year=" + YMD[0] + "&Month=" + YMD[1] + "&Day=" + YMD[2])		
-		data1 = conn.getresponse().read()
-		#f.write(data1 + "\n")
-		#f.close()
-		try:
-			latitude = data1.split('<a href="/glossary_e.html#latitude">Latitude</a>:')[1].split('<a href="/glossary_e.html#longitude">Longitude</a>:')[0]
-			latitude = latitude.split('<td>')[1].split('</td>')[0]
-			degree = float(latitude.split('<abbr title="degrees">&deg;</abbr>')[0])
-			minute = float(latitude.split('<abbr title="degrees">&deg;</abbr>')[1].split('<abbr title="minute">\'</abbr>')[0])
-			second = float(latitude.split('<abbr title="minute">\'</abbr>')[1].split('<abbr title="second">&quot;</abbr>')[0])
-			latitude = str(degree + minute/60.0 + second/3600.0)
-			longitude = data1.split('<a href="/glossary_e.html#longitude">Longitude</a>:')[1].split('<a href="/glossary_e.html#elevation">Elevation</a>:')[0]
-			longitude = longitude.split('<td>')[1].split('</td>')[0]	
-			degree = float(longitude.split('<abbr title="degrees">&deg;</abbr>')[0])
-			minute = float(longitude.split('<abbr title="degrees">&deg;</abbr>')[1].split('<abbr title="minute">\'</abbr>')[0])
-			second = float(longitude.split('<abbr title="minute">\'</abbr>')[1].split('<abbr title="second">&quot;</abbr>')[0])
-			longitude = str(-(degree + minute/60.0 + second/3600.0))
-			elevation = data1.split('<a href="/glossary_e.html#elevation">Elevation</a>:')[1].split('<a href="/glossary_e.html#climate_ID">Climate ID</a>:')[0]
-			elevation = elevation.split('<td>')[1].split('<abbr title="meter">m</abbr></td>')[0]
-			climateID = data1.split('<a href="/glossary_e.html#climate_ID">Climate ID</a>:')[1].split('<a href="/glossary_e.html#wmo_id"><abbr title="World Meteorological Organization">WMO</abbr> ID</a>:')[0]
-			climateID = climateID.split('<td>')[1].split('</td>')[0]
-			WMOID = data1.split('<a href="/glossary_e.html#wmo_id"><abbr title="World Meteorological Organization">WMO</abbr> ID</a>:')[1].split('<a href="/glossary_e.html#tc_ID"><abbr title="Transport Canada">TC</abbr> ID</a>:')[0]
-			WMOID = WMOID.split('<td>')[1].split('</td>')[0]
-			TCID = data1.split('<a href="/glossary_e.html#tc_ID"><abbr title="Transport Canada">TC</abbr> ID</a>:')[1].split('</tbody>')[0]
-			TCID = TCID.split('<td>')[1].split('</td>')[0]
-			results.append([StationName, StationID, hlyRange, dlyRange, mlyRange, latitude, longitude, elevation,  climateID, WMOID, TCID])
-		except ValueError:
-			results.append([StationName, StationID, hlyRange, dlyRange, mlyRange])
-		'''
-		#print StationName + "\t" + StationID + "\t" + hlyRange + "\t" + dlyRange + "\t" + mlyRange + "\t" + latitude + "\t" + longitude + "\t" + elevation  + "\t" + climateID + "\t" + WMOID + "\t" + TCID
-		#time.sleep(5)
+			print "hlyRange, dlyRange, mlyRange are all missing."
+			return []
+		data = urllib2.urlopen(url).read()
+		stationNameTag = '<th colspan="6" class="boxColor margin-bottom-none"><b>'
+		temp = data[data.find(stationNameTag) + len(stationNameTag):]
+		StationName = temp[:temp.find('<br />')].strip();
+		tag = '<a href="http://climate.weather.gc.ca/glossary_e.html#latitude">Latitude</a>:'
+		temp = data[data.find(tag) + len(tag):]
+		temp = temp[temp.find('<td>'):temp.find('</td>')].strip()
+		if len(temp) > 5:
+			Latitude = parseLatlng(temp);
+		else:
+			Latitude = '0.0'
+		tag = '<a href="http://climate.weather.gc.ca/glossary_e.html#longitude">Longitude</a>:'
+		temp = data[data.find(tag) + len(tag):]
+		temp = temp[temp.find('<td>'):temp.find('</td>')].strip()
+		if len(temp) > 5:
+			Longitude = str(-1*float(parseLatlng(temp)));
+		else:
+			Longitude = '0.0'
+		tag = '<a href="http://climate.weather.gc.ca/glossary_e.html#elevation">Elevation</a>:'
+		temp = data[data.find(tag) + len(tag):]
+		Elevation = temp[temp.find('<td>') + len('<td>'):temp.find('</td>')].strip();
+		if (Elevation.find('<abbr') > 0):
+			Elevation = Elevation[:Elevation.find('<abbr')].strip();
+		tag = '<a href="http://climate.weather.gc.ca/glossary_e.html#climate_ID">Climate ID</a>:'
+		temp = data[data.find(tag) + len(tag):]
+		ClimateID = temp[temp.find('<td>') + len('<td>'):temp.find('</td>')].strip();
+		tag = '<a href="http://climate.weather.gc.ca/glossary_e.html#wmo_id"><abbr title="World Meteorological Organization">WMO</abbr> ID</a>:'
+		temp = data[data.find(tag) + len(tag):]
+		WMO_ID = temp[temp.find('<td>') + len('<td>'):temp.find('</td>')].strip();
+		tag = '<a href="http://climate.weather.gc.ca/glossary_e.html#tc_ID"><abbr title="Transport Canada">TC</abbr> ID</a>:'
+		temp = data[data.find(tag) + len(tag):]
+		TC_ID = temp[temp.find('<td>') + len('<td>'):temp.find('</td>')].strip();
+		
+		time.sleep(2)
+		results.append([StationName, StationID, Latitude, Longitude, Elevation, TC_ID, "", ClimateID, WMO_ID])
 	return results
 
-# http://climate.weather.gc.ca/advanceSearch/searchHistoricDataStations_e.html?searchType=stnProv&timeframe=1&lstProvince=ONT&optLimit=yearRange&StartYear=1840&EndYear=2013&Year=2013&Month=9&Day=25&selRowPerPage=100&cmdProvSubmit=Search
-# http://climate.weather.gc.ca/advanceSearch/searchHistoricDataStations_e.html?searchType=stnProv&timeframe=1&lstProvince=ONT&optLimit=yearRange&StartYear=1840&EndYear=2013&Year=2013&Month=9&Day=27&selRowPerPage=100&cmdProvSubmit=Search&startRow=201
-#conn = httplib.HTTPConnection("lrcdrrvsdvap002")
-#conn.request("GET", "/web/page1.htm")
 conn = httplib.HTTPConnection("climate.weather.gc.ca")
-conn.request("GET", "/advanceSearch/searchHistoricDataStations_e.html?searchType=stnProv&timeframe=1&lstProvince=ONT&optLimit=yearRange&StartYear=1840&EndYear=2013&Year=2013&Month=9&Day=25&selRowPerPage=100&cmdProvSubmit=Search")
+conn.request("GET", "/advanceSearch/searchHistoricDataStations_e.html?searchType=stnProv&timeframe=1&lstProvince=ON&optLimit=yearRange&StartYear=1840&EndYear=2015&Year=2015&Month=7&Day=29&selRowPerPage=100&cmdProvSubmit=Search&startRow=1")
 r1 = conn.getresponse()
 data1 = r1.read()
-recordNumber = int(data1.split('locations match your customized search. Confirm the')[0].split('to share your comments and suggestions.</p>                </div></div><p>')[1])
+temp = data1[:data1.find('locations match your customized search. Confirm the')]
+recordNumber = int(temp[temp.rfind('>') + 1:])
 time.sleep(5)
 results = processOnePage(data1)
 startRow = 99
 while (startRow < recordNumber):
 	conn = httplib.HTTPConnection("climate.weather.gc.ca")
-	conn.request("GET", "/advanceSearch/searchHistoricDataStations_e.html?searchType=stnProv&timeframe=1&lstProvince=ONT&optLimit=yearRange&StartYear=1840&EndYear=2013&Year=2013&Month=9&Day=25&selRowPerPage=100&cmdProvSubmit=Search&startRow=" + str(startRow))
+	conn.request("GET", "/advanceSearch/searchHistoricDataStations_e.html?searchType=stnProv&timeframe=1&lstProvince=ON&optLimit=yearRange&StartYear=1840&EndYear=2015&Year=2015&Month=7&Day=29&selRowPerPage=100&cmdProvSubmit=Search&startRow=" + str(startRow))
 	results =  results + processOnePage(conn.getresponse().read())
 	time.sleep(2)
 	startRow = startRow + 100
+
+#print results
 f = open ("weather.txt","w")
-#f.write("\t".join(["StationName", "StationID", "hlyRange", "dlyRange", "mlyRange", "latitude", "longitude", "elevation", "climateID", "WMOID", "TCID"]) + "\n")
-#for result in results:
-	#print "\t".join(result)
-#	f.write("\t".join(result) + "\n")
-f.write("\n".join(results))
+f.write("\n".join(map(lambda row: "\t".join(row), results)))
 f.close()
-#print len(items)
--- Step2.py
+++ Step2.py
@@ -1,67 +1,74 @@
-# This script read the list.txt and a series of weather files
-# and create a file geodatabase with weather station feature class. 
-import sys, string, os
+# This script read the weather.txt and create a file geodatabase with weather station feature class. 
+
+import sys, string, os, time
 import arcpy
 reload(sys)
 sys.setdefaultencoding("latin-1")
 import zipfile
+start_time = time.time()
 INPUT_PATH = "input"
 OUTPUT_PATH = "output"
-os.system("ls " + INPUT_PATH + "//*.txt > " + INPUT_PATH + "//list.txt")
+#os.system("ls " + INPUT_PATH + "//*.txt > " + INPUT_PATH + "//list.txt")
 if arcpy.Exists(OUTPUT_PATH + "\\Climate.gdb"):
 	os.system("rmdir " + OUTPUT_PATH + "\\Climate.gdb /s /q")
 arcpy.CreateFileGDB_management(OUTPUT_PATH, "Climate", "9.3")
 OUTPUT_GDB = OUTPUT_PATH + "\\Climate.gdb"
-arcpy.CreateFeatureclass_management(OUTPUT_GDB, "WeatherStations", "POINT", "", "DISABLED", "DISABLED", "", "", "0", "0", "0")
-WeatherStations = OUTPUT_GDB + "\\WeatherStations"
-# Process: Define Projection
-arcpy.DefineProjection_management(WeatherStations, "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
-# Process: Add Fields	
-arcpy.AddField_management(WeatherStations, "STATIONNAME", "TEXT", "", "", "", "", "NON_NULLABLE", "REQUIRED", "")
-arcpy.AddField_management(WeatherStations, "ID", "LONG", "", "", "", "", "NULLABLE", "REQUIRED", "")
-arcpy.AddField_management(WeatherStations, "Latitude", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
-arcpy.AddField_management(WeatherStations, "Longitude", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
-arcpy.AddField_management(WeatherStations, "Elevation", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
-arcpy.AddField_management(WeatherStations, "Climate_ID", "TEXT", "", "", "", "", "NON_NULLABLE", "REQUIRED", "")
-arcpy.AddField_management(WeatherStations, "WMO_ID", "TEXT", "", "", "", "", "NON_NULLABLE", "REQUIRED", "")
-arcpy.AddField_management(WeatherStations, "TC_ID", "TEXT", "", "", "", "", "NON_NULLABLE", "REQUIRED", "")
-arcpy.AddField_management(WeatherStations, "COMMENTS", "TEXT", "", "", "", "", "NON_NULLABLE", "REQUIRED", "")
-f = open (INPUT_PATH + "\\list.txt","r")
+arcpy.env.workspace = OUTPUT_PATH + "\\Climate.gdb"
+
+def createFeatureClass(featureName, featureData, featureFieldList, featureInsertCursorFields):
+	print "Create " + featureName + " feature class"
+	featureNameNAD83 = featureName + "_NAD83"
+	featureNameNAD83Path = arcpy.env.workspace + "\\"  + featureNameNAD83
+	arcpy.CreateFeatureclass_management(arcpy.env.workspace, featureNameNAD83, "POINT", "", "DISABLED", "DISABLED", "", "", "0", "0", "0")
+	# Process: Define Projection
+	arcpy.DefineProjection_management(featureNameNAD83Path, "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
+	# Process: Add Fields	
+	for featrueField in featureFieldList:
+		arcpy.AddField_management(featureNameNAD83Path, featrueField[0], featrueField[1], featrueField[2], featrueField[3], featrueField[4], featrueField[5], featrueField[6], featrueField[7], featrueField[8])
+	# Process: Append the records
+	cntr = 1
+	try:
+		with arcpy.da.InsertCursor(featureNameNAD83, featureInsertCursorFields) as cur:
+			for rowValue in featureData:
+				cur.insertRow(rowValue)
+				cntr = cntr + 1
+	except Exception as e:
+		print "\tError: " + featureName + ": " + e.message
+	# Change the projection to web mercator
+	#arcpy.Project_management(featureNameNAD83Path, arcpy.env.workspace + "\\" + featureName, "PROJCS['WGS_1984_Web_Mercator_Auxiliary_Sphere',GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Mercator_Auxiliary_Sphere'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',0.0],PARAMETER['Standard_Parallel_1',0.0],PARAMETER['Auxiliary_Sphere_Type',0.0],UNIT['Meter',1.0]]", "NAD_1983_To_WGS_1984_5", "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
+	#arcpy.FeatureClassToShapefile_conversion([featureNameNAD83Path], OUTPUT_PATH + "\\Shapefile")
+	#arcpy.Delete_management(featureNameNAD83Path, "FeatureClass")
+	print "Finish " + featureName + " feature class."
+
+featureName = "EC_Weather_Stations"
+featureFieldList = [["STATIONNAME", "TEXT", "", "", "", "", "NON_NULLABLE", "REQUIRED", ""], ["ID", "LONG", "", "", "", "", "NULLABLE", "NON_REQUIRED", ""], ["Latitude", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", ""], ["Longitude", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", ""], ["Elevation", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", ""], ["TC_ID", "TEXT", "", "", "", "", "NULLABLE", "NON_REQUIRED", ""], ["COMMENTS", "TEXT", "", "", "", "", "NULLABLE", "NON_REQUIRED", ""], ["Climate_ID", "TEXT", "", "", "", "", "NULLABLE", "NON_REQUIRED", ""], ["WMO_ID", "TEXT", "", "", "", "", "NULLABLE", "NON_REQUIRED", ""]]
+featureInsertCursorFields = tuple(["SHAPE@XY"] + map(lambda list: list[0],featureFieldList))
+
+f = open (INPUT_PATH + "\\weather.txt","r")
 stations = f.read().split("\n")
 f.close()
-stations = map(lambda station: station[7:-4], stations)
-#print stations
-arcpy.env.workspace = OUTPUT_GDB
-try:
-	with arcpy.da.InsertCursor("WeatherStations", ("SHAPE@XY", "STATIONNAME", "ID", "Latitude", "Longitude", "Elevation", "Climate_ID", "WMO_ID", "TC_ID", "COMMENTS")) as cur:
-		cntr = 1
-		#print stations
-		for StationID in stations:
-			#print StationID
-			f = open (INPUT_PATH + "\\" + StationID + ".txt","r")
-			data = f.read()
-			f.close()
-			#print StationID
-			items = data.split("\n")
-			StationName = items[0].split("\",\"")[1][:-1]
-			Latitude = float(items[2].split("\",\"")[1][:-1])
-			Longitude = float(items[3].split("\",\"")[1][:-1])
-			Elevation = items[4].split("\",\"")[1][:-1]
-			if(len(Elevation) > 0):
-				Elevation = float(Elevation)
-			else:
-				Elevation = float('nan')
-			ClimateIdentifier = items[5].split("\",\"")[1][:-1]
-			WMOIdentifier = items[6].split("\",\"")[1][:-1]
-			TCIdentifier = items[7].split("\",\"")[1][:-1]
-			Comments = ""
-			if StationID == "4490":
-				Latitude = 45.348
-				Longitude = -80.035
-				Comments = "Lat&Lng are calculated using Parry Sound."
-			rowValue = [(Longitude, Latitude), StationName, StationID, Latitude, Longitude, Elevation, ClimateIdentifier, WMOIdentifier, TCIdentifier, Comments]
-			cur.insertRow(rowValue)			
-			#print StationID + "\t" + StationName + "\t" + Latitude + "\t" + Longitude + "\t" + Elevation + "\t" + ClimateIdentifier + "\t" + WMOIdentifier + "\t" + TCIdentifier
-except Exception as e:
-	print e.message
+def getRow(line):
+	items = line.split('\t')
+	items = map(lambda item: item.replace("&nbsp;", ""), items)
+	#print items
+	stationName = items[0]
+	stationID = int(items[1])
+	latitude = float(items[2])
+	longitude = float(items[3])
+	elevation = None if len(items[4]) == 0 else float(items[4])
+	TC_ID = items[5]
+	COMMENTS = items[6]
+	Climate_ID = items[7]
+	WMO_ID = items[8]
+	if stationID == 4490:
+		latitude = 45.348
+		longitude = -80.035
+		COMMENTS = "Lat&Lng are calculated using Parry Sound."	
+	return [(longitude, latitude), stationName, stationID, latitude, longitude, elevation, TC_ID, COMMENTS, Climate_ID, WMO_ID]
+rows = map(getRow, stations);
+createFeatureClass(featureName, rows, featureFieldList, featureInsertCursorFields)
+
+elapsed_time = time.time() - start_time
+print elapsed_time
+
 
